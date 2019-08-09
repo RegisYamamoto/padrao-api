36c519463d2cb2c455cbf72712bad58059320d82
regisyamamoto <regisyamamoto@gmail.com>
2019-08-09 16:50:51 -0300

    Implementado o endpoin processUploadFile

--- src/main/java/com/regis/padraoapi/controller/MarcController.java
+++ src/main/java/com/regis/padraoapi/controller/MarcController.java
@@ -1,17 +1,23 @@
 package com.regis.padraoapi.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.regis.padraoapi.dto.MarcDTO;
 import com.regis.padraoapi.model.Marc;
@@ -27,6 +33,8 @@ public class MarcController {
 
 	@Autowired
 	private MarcService marcService;
+	
+	String dirCampanhas = "C:/Workspace/padrao-api/uploaded";
 	
 	@ApiOperation(value="Cadastra uma marca")
 	@PostMapping("")
@@ -47,6 +55,22 @@ public class MarcController {
 		List<Marc> list = marcService.listarMarcas();
 		List<MarcDTO> listDto = list.stream().map(obj -> new MarcDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value = "/uploadFile", method = RequestMethod.POST, consumes = {"multipart/form-data"})
+	public ResponseEntity<?> processUploadFile(@RequestParam(value = "file") MultipartFile file) {
+		File store = null;
+
+		try {
+			store = new File(dirCampanhas + "/" + file.getOriginalFilename());
+
+			file.transferTo(store);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return new ResponseEntity<Object>(String.format(store.getAbsolutePath()), HttpStatus.OK);
 	}
 	
 }
